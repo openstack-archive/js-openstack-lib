58c28cb158c31b733837f8f185c686db75f801f2
Vitaly Kramskikh <vkramskikh@mirantis.com>
2016-07-14 18:07:36 +0300

    Enforce ES2015 syntax
    The whole project is going to use ES2015, so it's important to
    enforce usage of the new syntax to the project code homogeneous.
    This commit enforces usage of arrow functions where possible,
    let/const instead of var and rest/spread operator instead of
    arguments and call/apply.
    
    Change-Id: I2fd33400d7d4a3ed23363ceaaf0c2d2f1bd55c47

--- test/unit/indexTest.js
+++ test/unit/indexTest.js
@@ -2,7 +2,7 @@ import Test from "../../src/index.js";
 
 describe("Simple test", () => {
   it("should export a class", () => {
-    var t = new Test();
+    let t = new Test();
     expect(t).toBeDefined();
   });
 });
